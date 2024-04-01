019536ac204761ca45e1b1e4de6f57e8221101f1
Alexandra Veney Maryville <aveney1@live.maryville.edu>
2024-04-01 16:15:16 -0400

    comment added

--- weekday.js
+++ weekday.js
@@ -1,7 +1,7 @@
 var http = require("http")
 
 http.createServer(function (request, response) {
-//Change for file
+//Change for files
    // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
